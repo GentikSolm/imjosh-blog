a977432ebb7d3b2f2199fad722849e950ae2e1dd
josh <josh@mjbrowns.com>
2023-10-28 00:23:07 -0500

    feat: initial page load animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,16 @@ export default {
         sans: ["var(--font-sans)", ...defaultTheme.fontFamily.sans],
       },
     },
+    keyframes: {
+      slideUp: {
+        "0%": { transform: "translateY(1em)", opacity: "0" },
+        "100%": { transform: "translateY(0)", opacity: "100" },
+      },
+    },
+    animation: {
+      slideUp: "slideUp .5s ease-in-out",
+    },
   },
-  darkMode: 'class',
-  plugins: [
-    require("@tailwindcss/typography"),
-  ],
+  darkMode: "class",
+  plugins: [require("@tailwindcss/typography")],
 } satisfies Config;
