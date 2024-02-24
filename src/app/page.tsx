eafb2038652288e231e1e4222508016aa22bceb2
josh <josh@mjbrowns.com>
2024-02-23 23:30:57 -0600

    feat: update blog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,48 @@ export default function Page() {
       </h1>
       <div className="max-w-2xl">
         <p>
-          Im a software engineer based out of Austin TX and currently CTO at{" "}
-          <a href="https://biblish.com" className="hover:underline">
-            Biblish
+          Im a software engineer based out of Austin TX. I am currently the
+          Software Lead at{" "}
+          <a href="https://manifold.inc" className="hover:underline">
+            Manifold Labs
           </a>
-          , where we are building a home for literature in the 21st century.
-          Check our current project,{" "}
-          <a href="https://papertrail.biblish.com">Papertrail</a>!
+          , where we are building the future of search Check our current
+          project, <a href="https://sybil.com">Sybil</a>!
         </p>
-        <h3>A bit of background</h3>
+        <h3>Past Work</h3>
+        <p>
+          From mid 2022 to 2024 I was proudly the CTO at{" "}
+          <a href="https://biblish.com">Biblish</a>, where we built the home of
+          literature for the 21st century. During my time at biblish we launched{" "}
+          <a href="https://papertrail.biblish.com">Papertrail</a> and{" "}
+          <a href="https://submissions.biblish.com">Submissions</a>.
+        </p>
+        <p>
+          In 2023 I formed my own LLC called{" "}
+          <a href="https://brunuslabs.com">Brunus Labs</a> to take on contract
+          work. Since I started Brunus we
+          <sup>
+            <a href="#footnote-1" className="pb-1 text-xs no-underline">
+              [1]
+            </a>
+          </sup>{" "}
+          have built from internal time tracking applications, lightweight POS
+          systems, all the way to discord bots and Marketing sites.
+        </p>
+        <p>
+          Before Brunus Labs I was a Web and Application Developer at Cleveland
+          Clinic. I created multiple web applications, one of which to this day
+          saves the clinic more than 30k per year in licencing fees. I also
+          moved all of our teams projects from TFVC to git, along with being the
+          first to introduce and setup CICD, inclduing automatic deployments,
+          concurrent builds, code linting, and more.
+        </p>
+        <p id="footnote-1" className="text-sm">
+          [1]: I was able to hire 3 of my friends under Brunus Labs during
+          college. Matt, Ahmed and Will helped tremendously in not only building
+          Brunus Labs but also helping me become the developer I am today.
+        </p>
+        <h3>Personal Background</h3>
         <p>
           Born and raised in Ohio, I always loved computers and technology. I
           first was introduced to programming in 6th grade where I started
@@ -39,7 +72,9 @@ export default function Page() {
         <p>
           I love motivating people to build cool stuff, and encourage people to
           never stop learning. I like giving talks to classes at universities,
-          and being involived in the tech community.
+          and being involived in the tech community. If your interested in
+          having me talk at your univeristy or other event, shoot me an{" "}
+          <a href="mailto:hey@imjosh.dev">email</a>!
         </p>
         <p>
           I also love helping out with open source projects where I can. You can
